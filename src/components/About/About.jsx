faf2106649bea2dea258c7c390c3ec5628ef8e0c
ivan-niceman <sn.iv@bk.ru>
2024-10-02 20:07:31 +0700

    change: photos, price. add: new feedback

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,10 @@
-import { useState, useRef } from "react";
-import { Link } from "react-router-dom";
+import { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
-import img1 from "../../images/islands1.jpeg";
-import img2 from "../../images/islands2.jpeg";
-import certificate from "../../images/certificate.png";
+import img1 from '../../images/islands1.webp';
+import img2 from '../../images/islands2.jpeg';
+import certificate from '../../images/certificate.png';
 
 export default function About() {
   const [showCertificate, setShowCertificate] = useState(false);
@@ -12,7 +12,11 @@ export default function About() {
   const nodeRef = useRef(null);
 
   const handleOverlayClick = (evt) => {
-    if (evt.target === evt.currentTarget || evt.target.classList.contains("about__popup-certificate") || evt.target.classList.contains("about__popup-more")) {
+    if (
+      evt.target === evt.currentTarget ||
+      evt.target.classList.contains('about__popup-certificate') ||
+      evt.target.classList.contains('about__popup-more')
+    ) {
       setShowCertificate(false);
       setShowMore(false);
     }
@@ -56,71 +60,136 @@ export default function About() {
               рекламных кампаний наших партнеров.
             </p>
             <p className="about__paragraph">
-            С 2024 года группа является официальным членом Российского союза
+              С 2024 года группа является официальным членом Российского союза
               туриндустрии, что подтверждается соответствующим&nbsp;
-            <button
-              className="about__paragraph-certificate"
-              onClick={() => setShowCertificate(true)}
-            >
-              сертификатом.
-            </button>
+              <button
+                className="about__paragraph-certificate"
+                onClick={() => setShowCertificate(true)}
+              >
+                сертификатом.
+              </button>
             </p>
           </div>
           <img src={img1} alt="море и пляж" className="about__img" />
           <img src={img2} alt="море и пляж" className="about__img" />
           <div className="about__contacts">
-            <Link to={`https://www.facebook.com/groups/we.are.islands`} className="about__link" target="_blank">
+            <Link
+              to={`https://www.facebook.com/groups/we.are.islands`}
+              className="about__link"
+              target="_blank"
+            >
               Перейти в facebook<sup>*</sup>
             </Link>
-            <Link to={`https://t.me/+X9rH3EbMiZZiYzc6`} className="about__link" target="_blank">
+            <Link
+              to={`https://t.me/+X9rH3EbMiZZiYzc6`}
+              className="about__link"
+              target="_blank"
+            >
               Перейти в telegram
             </Link>
           </div>
         </div>
         <div className="about__banner">
-          <h3 className="about__banner-subtitle">ОСТРОВА и Мы — МИР — расширяем географию!</h3>
-          <button className="about__banner-button button"
-          onClick={() => setShowMore(true)}
-          >читать подробнее </button>
+          <h3 className="about__banner-subtitle">
+            ОСТРОВА и Мы — МИР — расширяем географию!
+          </h3>
+          <button
+            className="about__banner-button button"
+            onClick={() => setShowMore(true)}
+          >
+            читать подробнее{' '}
+          </button>
         </div>
       </div>
-      <CSSTransition in={showCertificate} timeout={300} classNames="alert" unmountOnExit nodeRef={nodeRef}>
+      <CSSTransition
+        in={showCertificate}
+        timeout={300}
+        classNames="alert"
+        unmountOnExit
+        nodeRef={nodeRef}
+      >
         <div className="about__popup-certificate" ref={nodeRef}>
-        <span>
-          <img
-            src={certificate}
-            alt="сертификат"
-            className="about__certificate"
-          />
-          <button
-            className="about__popup-close-button"
-            onClick={() => setShowCertificate(false)}
-          />
-        </span>
+          <span>
+            <img
+              src={certificate}
+              alt="сертификат"
+              className="about__certificate"
+            />
+            <button
+              className="about__popup-close-button"
+              onClick={() => setShowCertificate(false)}
+            />
+          </span>
         </div>
       </CSSTransition>
-      <CSSTransition in={showMore} timeout={300} classNames="alert" unmountOnExit nodeRef={nodeRef}>
-      <div className="about__popup-more" ref={nodeRef}>
-      <div className="about__popup-more-section">
-        <h2 className="about__popup-more-title">ОСТРОВА и Мы - МИР - расширяем географию!</h2>
-        <div className="about__popup-more-content">
-          <span className="about__popup-more-info">
-            <p>В 2020 году мы основали группу &quot;Острова и мы&quot; с амбициозной целью создать сообщество, объединяющее профессионалов туристической индустрии, специализирующихся на островных направлениях. Начиная с ограниченного числа направлений, мы не ожидали такого стремительного роста активности, который свидетельствовал о большом интересе к такой нишевой специализации. Мы стремились не только собрать единомышленников, но и создать платформу для обмена знаниями и опытом без конкуренции, где важны поддержка, взаимовыручка, честность и уважение.</p>
-            <p>С течением времени, запросы участников начали выходить за рамки исключительно островного туризма. Возникла потребность рассмотрения ребрендинга, чтобы адаптировать группу к расширяющимся интересам без потери нашей основной концепции. Размышления о новой  идентичности привели к случайной ошибке в написании названия группы – &quot;Острова и мир&quot;, что оказалось революционным. Это открытие подтолкнуло нас к решению расширить наше видение, включив не только островные направления, но и другие экзотические и эксклюзивные локации, предлагающие уникальные, премиальные виды отдыха.</p>
-            <p>Эта эволюция позволила нам сохранить верность нашим корням, углубляя и расширяя нашу миссию и предложение для сообщества. Теперь &quot;Острова и мир&quot; не просто форум, где обсуждаются островные направления; это место, где профессионалы могут обмениваться опытом о различных уникальных и эксклюзивных направлениях. Это стало возможным благодаря приверженности наших участников и их стремлению поддерживать друг друга в поиске наилучших туристических решений.</p>
-            <p>Также мы заметили, как важно наше сообщество для коллег, которые стремятся решать профессиональные вопросы в дружественной и поддерживающей атмосфере. Многие коллеги выразили желание продолжать реализацию проектов именно с нами, чувствуя надежность и отсутствие осуждения, что вдохновляет нас продолжать нашу работу и развивать сообщество дальше.</p>
-          </span>
-          <span className="about__popup-more-images">
-            <img src={img2} alt="острова и мы" />
-          </span>
+      <CSSTransition
+        in={showMore}
+        timeout={300}
+        classNames="alert"
+        unmountOnExit
+        nodeRef={nodeRef}
+      >
+        <div className="about__popup-more" ref={nodeRef}>
+          <div className="about__popup-more-section">
+            <h2 className="about__popup-more-title">
+              ОСТРОВА и Мы - МИР - расширяем географию!
+            </h2>
+            <div className="about__popup-more-content">
+              <span className="about__popup-more-info">
+                <p>
+                  В 2020 году мы основали группу &quot;Острова и мы&quot; с
+                  амбициозной целью создать сообщество, объединяющее
+                  профессионалов туристической индустрии, специализирующихся на
+                  островных направлениях. Начиная с ограниченного числа
+                  направлений, мы не ожидали такого стремительного роста
+                  активности, который свидетельствовал о большом интересе к
+                  такой нишевой специализации. Мы стремились не только собрать
+                  единомышленников, но и создать платформу для обмена знаниями и
+                  опытом без конкуренции, где важны поддержка, взаимовыручка,
+                  честность и уважение.
+                </p>
+                <p>
+                  С течением времени, запросы участников начали выходить за
+                  рамки исключительно островного туризма. Возникла потребность
+                  рассмотрения ребрендинга, чтобы адаптировать группу к
+                  расширяющимся интересам без потери нашей основной концепции.
+                  Размышления о новой идентичности привели к случайной ошибке в
+                  написании названия группы – &quot;Острова и мир&quot;, что
+                  оказалось революционным. Это открытие подтолкнуло нас к
+                  решению расширить наше видение, включив не только островные
+                  направления, но и другие экзотические и эксклюзивные локации,
+                  предлагающие уникальные, премиальные виды отдыха.
+                </p>
+                <p>
+                  Эта эволюция позволила нам сохранить верность нашим корням,
+                  углубляя и расширяя нашу миссию и предложение для сообщества.
+                  Теперь &quot;Острова и мир&quot; не просто форум, где
+                  обсуждаются островные направления; это место, где
+                  профессионалы могут обмениваться опытом о различных уникальных
+                  и эксклюзивных направлениях. Это стало возможным благодаря
+                  приверженности наших участников и их стремлению поддерживать
+                  друг друга в поиске наилучших туристических решений.
+                </p>
+                <p>
+                  Также мы заметили, как важно наше сообщество для коллег,
+                  которые стремятся решать профессиональные вопросы в
+                  дружественной и поддерживающей атмосфере. Многие коллеги
+                  выразили желание продолжать реализацию проектов именно с нами,
+                  чувствуя надежность и отсутствие осуждения, что вдохновляет
+                  нас продолжать нашу работу и развивать сообщество дальше.
+                </p>
+              </span>
+              <span className="about__popup-more-images">
+                <img src={img2} alt="острова и мы" />
+              </span>
+            </div>
+            <button
+              className="about__popup-close-button"
+              onClick={() => setShowMore(false)}
+            />
+          </div>
         </div>
-        <button
-            className="about__popup-close-button"
-            onClick={() => setShowMore(false)}
-          />
-      </div>
-    </div>
-    </CSSTransition>
+      </CSSTransition>
     </section>
   );
 }
